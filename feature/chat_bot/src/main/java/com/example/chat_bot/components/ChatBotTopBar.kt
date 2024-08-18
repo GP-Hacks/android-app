fb6348b4768380a25367a43191a1889fe49ab076
qwertyAL <sergeyiksanov25@gmail.com>
2024-08-18 17:34:34 +0300

    add chat bot top bar

---
+++ feature/chat_bot/src/main/java/com/example/chat_bot/components/ChatBotTopBar.kt
@@ -0,0 +1,74 @@
+package com.example.chat_bot.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ui.theme.mColors
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ChatBotTopBar(
+    modifier: Modifier = Modifier,
+    title: String,
+    additionalInfo: String,
+    image: String,
+    onBackPressed: () -> Unit,
+    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors()
+) {
+
+    TopAppBar(
+        title = {
+            Column {
+                Text(text = title, color = Color.Black, fontWeight = FontWeight.Medium, fontSize = 20.sp)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(6.dp)
+                            .background(mColors.primary, CircleShape)
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(text = additionalInfo, color = mColors.primary, fontWeight = FontWeight.Normal, fontSize = 14.sp)
+                }
+            }
+        },
+        actions = {
+            Box(
+                modifier = Modifier
+                    .size(38.dp)
+                    .background(mColors.primary, CircleShape)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+        },
+        modifier = modifier,
+        navigationIcon = {
+            IconButton(onClick = onBackPressed) {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null, tint = Color.Black)
+            }
+        },
+        colors = colors
+    )
+
+}
