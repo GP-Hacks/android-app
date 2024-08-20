c8f67ec5d13f54ba41450162463f1cb7705c30e8
qwertyAL <sergeyiksanov25@gmail.com>
2024-08-21 02:56:02 +0300

    add base notifications

---
+++ app/src/main/java/com/example/tatarstanresidentcard/notifications/NotificationService.kt
@@ -0,0 +1,87 @@
+package com.example.tatarstanresidentcard.notifications
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.tatarstanresidentcard.MainActivity
+import com.example.tatarstanresidentcard.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class NotificationService: FirebaseMessagingService() {
+
+    private val tag = "NOTIFICATIONS_FCM"
+    private val channelId = "tatarstan_resident_card_notification_channel_id"
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+
+        Log.i("TOKEN", token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+
+        Log.i("MESSAGE NFCM", message.data.toString())
+
+        if (message.data.isNotEmpty()) {
+
+            createNotificationChannel()
+
+            val intent = Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+            val builder = NotificationCompat.Builder(applicationContext, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(message.data["title"])
+                .setContentText(message.data["content"])
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+
+            with(NotificationManagerCompat.from(applicationContext)) {
+                if (ActivityCompat.checkSelfPermission(
+                        applicationContext,
+                        Manifest.permission.POST_NOTIFICATIONS
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                notify(101, builder.build())
+            }
+
+        }
+
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "tatarstan resident card notification channel"
+            val description = "notification channel for tatarstan resident card"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                this.description = description
+            }
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).createNotificationChannel(channel)
+        }
+    }
+
+}
