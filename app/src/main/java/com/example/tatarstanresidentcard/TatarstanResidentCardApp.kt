64ef0c9227517d4faa9f2d80ecb570ba899713c2
qwertyAL <sergeyiksanov25@gmail.com>
2024-08-21 02:55:25 +0300

    update TatarstanResidentCardApp.kt

--- app/src/main/java/com/example/tatarstanresidentcard/TatarstanResidentCardApp.kt
+++ app/src/main/java/com/example/tatarstanresidentcard/TatarstanResidentCardApp.kt
@@ -1,14 +1,18 @@
 package com.example.tatarstanresidentcard
 
+ import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import com.example.chat_bot.navigation.ChatBot
+import com.example.home.navigation.Home
 import com.example.tatarstanresidentcard.navigation.TatarstanResidentCardNavHost
 
 @Composable
 fun TatarstanResidentCardApp(
     modifier: Modifier = Modifier,
-    startDestination: Any = ChatBot
+    startDestination: Any = Home
 ) {
     TatarstanResidentCardNavHost(
         startDestination = startDestination,
