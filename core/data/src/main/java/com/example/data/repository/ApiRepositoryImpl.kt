4bbe91f5ca5e217aa5c96cd0e595148638589dfe
qwertyAL <sergeyiksanov25@gmail.com>
2024-08-21 02:50:00 +0300

    add ApiRepositoryImpl

---
+++ core/data/src/main/java/com/example/data/repository/ApiRepositoryImpl.kt
@@ -0,0 +1,72 @@
+package com.example.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.common.model.ResultModel
+import com.example.data.response.toChatBotAnswerModel
+import com.example.data.response.toPlaceModel
+import com.example.data.source.paging.NewsPagingSource
+import com.example.data.source.remote.ApiRemoteSource
+import com.example.data.utils.millisToUtcString
+import com.example.domain.model.ChatBotAnswerModel
+import com.example.domain.model.NewsModel
+import com.example.domain.model.PlaceModel
+import com.example.domain.repository.ApiRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class ApiRepositoryImpl @Inject constructor(
+    private val apiRemoteSource: ApiRemoteSource
+): ApiRepository {
+    override fun getPlaces(
+        latitude: Double,
+        longitude: Double,
+        category: String
+    ): Flow<ResultModel<List<PlaceModel>>> = flow {
+        emit(ResultModel.loading())
+
+        val result = apiRemoteSource.getListPlaces(latitude, longitude, category)
+
+        if (result.status == ResultModel.Status.SUCCESS) {
+            emit(ResultModel.success(result.data!!.response.map { it.toPlaceModel() }))
+        } else {
+            emit(ResultModel.failure(result.message))
+        }
+    }
+
+    override fun getNews(pageSize: Int): Flow<PagingData<NewsModel>> = Pager(
+        config = PagingConfig(pageSize = pageSize, prefetchDistance = 2),
+        pagingSourceFactory = {
+            NewsPagingSource(
+                apiRemoteSource = apiRemoteSource,
+                pageSize = pageSize
+            )
+        }
+    ).flow
+
+    override fun getChatBotAnswerToUserRequest(userRequest: String): Flow<ResultModel<ChatBotAnswerModel>> = flow {
+        emit(ResultModel.loading())
+
+        val result = apiRemoteSource.getChatBotAnswerToUserRequest(userRequest)
+
+        if (result.status == ResultModel.Status.SUCCESS) {
+            emit(ResultModel.success(result.data!!.toChatBotAnswerModel()))
+        } else {
+            emit(ResultModel.failure(result.message))
+        }
+    }
+
+    override fun buyTicketForPlace(id: Int, date: Long, time: Long): Flow<ResultModel<Boolean>> = flow {
+        emit(ResultModel.loading())
+
+        val result = apiRemoteSource.buyTicketForPlace(id, millisToUtcString(date + time))
+
+        if (result.status == ResultModel.Status.SUCCESS) {
+            emit(ResultModel.success(result.data!!))
+        } else {
+            emit(ResultModel.failure(result.message))
+        }
+    }
+}
