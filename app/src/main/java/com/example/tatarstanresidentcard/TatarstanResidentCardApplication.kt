024899fc4f48fd767e4316c6d4013fc1e15b75a2
qwertyAL <sergeyiksanov25@gmail.com>
2024-08-21 02:55:38 +0300

    update TatarstanResidentCardApplication

--- app/src/main/java/com/example/tatarstanresidentcard/TatarstanResidentCardApplication.kt
+++ app/src/main/java/com/example/tatarstanresidentcard/TatarstanResidentCardApplication.kt
@@ -1,7 +1,16 @@
 package com.example.tatarstanresidentcard
 
 import android.app.Application
+import com.yandex.mapkit.MapKitFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TatarstanResidentCardApplication: Application()
+class TatarstanResidentCardApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        MapKitFactory.setApiKey("1681f575-a055-4d87-9f68-9706c335baa0")
+    }
+
+}
