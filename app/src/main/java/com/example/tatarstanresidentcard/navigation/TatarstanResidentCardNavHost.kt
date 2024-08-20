f652aacd6c145eabaa94e5261b0cebcff203e53f
qwertyAL <sergeyiksanov25@gmail.com>
2024-08-21 02:55:10 +0300

    update navigation

--- app/src/main/java/com/example/tatarstanresidentcard/navigation/TatarstanResidentCardNavHost.kt
+++ app/src/main/java/com/example/tatarstanresidentcard/navigation/TatarstanResidentCardNavHost.kt
@@ -1,27 +1,197 @@
 package com.example.tatarstanresidentcard.navigation
 
+import android.util.Log
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.chat_bot.navigation.ChatBot
 import com.example.chat_bot.navigation.chatBotScreen
+import com.example.chat_bot.navigation.navigateToChatBot
+import com.example.home.navigation.Home
+import com.example.home.navigation.homeScreen
+import com.example.home.navigation.navigateToHome
+import com.example.news.navigation.News
+import com.example.news.navigation.navigateToNews
+import com.example.news.navigation.newsScreen
+import com.example.places.navigation.navigateToPlaces
+import com.example.places.navigation.placesScreen
+import com.example.services.navigation.Services
+import com.example.services.navigation.navigateToServices
+import com.example.services.navigation.servicesScreen
+import com.example.stocks.navigation.Stocks
+import com.example.stocks.navigation.navigateToStocks
+import com.example.stocks.navigation.stocksScreen
+import com.example.tatarstanresidentcard.R
+import com.example.ui.theme.mColors
 
 @Composable
 fun TatarstanResidentCardNavHost(
     modifier: Modifier = Modifier,
-    startDestination: Any = ChatBot,
+    startDestination: Any = Home,
     navController: NavHostController = rememberNavController()
 ) {
-    NavHost(
-        modifier = modifier,
-        navController = navController,
-        startDestination = startDestination
+    var currentRoute by remember {
+        mutableStateOf(startDestination)
+    }
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRouteReal = currentBackStackEntry?.destination?.route
+    val bottomBarVisible = when (currentRouteReal) {
+        "com.example.home.navigation.Home" -> true
+        "com.example.services.navigation.Services" -> true
+        "com.example.stocks.navigation.Stocks" -> true
+        "com.example.news.navigation.News" -> true
+        "com.example.places.Places" -> true
+        else -> false
+    }
+    Log.i("ROUTE CURR", currentRouteReal.toString())
+
+    Scaffold(
+        bottomBar = { if (bottomBarVisible) BottomNavigationBar(navController = navController, currentRoute = currentRoute, changeRoute = { currentRoute = it }) }
     ) {
-
-        chatBotScreen()
-
+        NavHost(
+            modifier = modifier.padding(bottom = it.calculateBottomPadding()),
+            navController = navController,
+            startDestination = startDestination,
+            enterTransition = { EnterTransition.None },
+            exitTransition = { ExitTransition.None },
+            popExitTransition = { ExitTransition.None },
+            popEnterTransition = { EnterTransition.None}
+        ) {
+            chatBotScreen()
+            homeScreen()
+            newsScreen()
+            servicesScreen(
+                navigateToPlaces = {
+                    navController.navigateToPlaces()
+                },
+                navigateToReadersDiary = {},
+                navigateToVoting = {},
+                navigateToSchoolElectronDiary = {},
+                navigateToStocks = {},
+                navigateToPortalCare = {}
+            )
+            stocksScreen()
+            placesScreen()
+        }
     }
 
+}
+
+@Composable
+fun BottomNavigationBar(
+    navController: NavController,
+    currentRoute: Any,
+    changeRoute: (Any) -> Unit
+) {
+    NavigationBar(
+        containerColor = Color.Transparent
+    ) {
+        NavigationBarItem(
+            selected = currentRoute is Home,
+            onClick = {
+                changeRoute(Home)
+                navController.navigateToHome()
+            },
+            label = {
+                Text(text = "Главная")
+            },
+            colors = NavigationBarItemDefaults.colors(
+                selectedIconColor = mColors.primary,
+                selectedTextColor = mColors.primary,
+                indicatorColor = Color.Transparent,
+                unselectedIconColor = mColors.onTertiary,
+                unselectedTextColor = mColors.onTertiary
+            ),
+            icon = { Icon(painter = painterResource(id = R.drawable.house_icon), contentDescription = null) }
+        )
+        NavigationBarItem(
+            selected = currentRoute is Services,
+            onClick = { 
+                changeRoute(Services)
+                navController.navigateToServices()
+            },
+            label = {
+                Text(text = "Сервисы")
+            },
+            colors = NavigationBarItemDefaults.colors(
+                selectedIconColor = mColors.primary,
+                selectedTextColor = mColors.primary,
+                indicatorColor = Color.Transparent,
+                unselectedIconColor = mColors.onTertiary,
+                unselectedTextColor = mColors.onTertiary
+            ),
+            icon = { Icon(painter = painterResource(id = R.drawable.services_icon), contentDescription = null) }
+        )
+        ElevatedButton(
+            modifier = Modifier.size(50.dp),
+            onClick = { navController.navigateToChatBot() },
+            shape = RoundedCornerShape(15.dp),
+            elevation = ButtonDefaults.buttonElevation(4.dp),
+            colors = ButtonDefaults.elevatedButtonColors(
+                containerColor = mColors.onSecondary,
+                contentColor = Color.Transparent
+            )
+        ) {
+            Icon(painter = painterResource(id = R.drawable.stocks_icon), contentDescription = null)
+        }
+        NavigationBarItem(
+            selected = currentRoute is Stocks,
+            onClick = {
+                changeRoute(Stocks)
+                navController.navigateToStocks()
+            },
+            label = {
+                Text(text = "Акции")
+            },
+            colors = NavigationBarItemDefaults.colors(
+                selectedIconColor = mColors.primary,
+                selectedTextColor = mColors.primary,
+                indicatorColor = Color.Transparent,
+                unselectedIconColor = mColors.onTertiary,
+                unselectedTextColor = mColors.onTertiary
+            ),
+            icon = { Icon(painter = painterResource(id = R.drawable.stocks_icon), contentDescription = null) }
+        )
+        NavigationBarItem(
+            selected = currentRoute is News,
+            onClick = {
+                changeRoute(News)
+                navController.navigateToNews()
+            },
+            label = {
+                Text(text = "Новости")
+            },
+            colors = NavigationBarItemDefaults.colors(
+                selectedIconColor = mColors.primary,
+                selectedTextColor = mColors.primary,
+                indicatorColor = Color.Transparent,
+                unselectedIconColor = mColors.onTertiary,
+                unselectedTextColor = mColors.onTertiary
+            ),
+            icon = { Icon(painter = painterResource(id = R.drawable.news_icon), contentDescription = null) }
+        )
+    }
 }
