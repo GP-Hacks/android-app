b3c33036e8702dfcb9b91863e3a77c6b51314047
qwertyAL <sergeyiksanov25@gmail.com>
2024-08-21 02:54:53 +0300

    update :feature:stocks

---
+++ feature/stocks/src/main/java/com/example/stocks/navigation/StocksNavigation.kt
@@ -0,0 +1,23 @@
+package com.example.stocks.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.NavOptionsBuilder
+import androidx.navigation.compose.composable
+import com.example.stocks.StocksRoute
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Stocks
+
+fun NavController.navigateToStocks(
+    navOptions: NavOptionsBuilder.() -> Unit = {}
+) = navigate(Stocks, navOptions)
+
+fun NavGraphBuilder.stocksScreen(
+//    changeVisibleNavBar: () -> Unit
+) = composable<Stocks> {
+//    changeVisibleNavBar()
+    StocksRoute()
+}
