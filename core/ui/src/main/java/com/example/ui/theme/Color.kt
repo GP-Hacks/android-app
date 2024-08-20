7fdebdcfd376c5493b6c65130526951ec2dbcfd3
qwertyAL <sergeyiksanov25@gmail.com>
2024-08-21 02:52:29 +0300

    update colors in theme

--- core/ui/src/main/java/com/example/ui/theme/Color.kt
+++ core/ui/src/main/java/com/example/ui/theme/Color.kt
@@ -2,11 +2,11 @@ package com.example.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val BrightGray = Color(0xFFE5E5E5) // Surface
+val BrightGray = Color(0xFFF2F2F2) // Surface
 val Black = Color(0xFF000000) // OnSurface
 val White = Color(0xFFFFFFFF) // SurfaceContainer
 val Gray = Color(0xFFEEEEEE) // Background
-val Green = Color(0xFF00B545) // Primary
-val LightSkyBlue = Color(0xFF5EA3DE) // PrimaryContainer
-val LightSlateGray = Color(0xFF7D8B98) // Secondary
-val SteelBlue = Color(0xFF3E5262) // SecondaryContainer
+val Green = Color(0xFF00B545)
+val GrayGray = Color(0xFFDBDBDB)
+val GrayGrayGray = Color(0xFFABABAB)
+val GrayGrayGrayGray = Color(0xFFF8F8F8)
