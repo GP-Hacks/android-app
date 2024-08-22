ab1801e067822a7451b46b926c7b7ff4f52d2da1
qwertyAL <sergeyiksanov25@gmail.com>
2024-08-22 10:19:35 +0300

    add CharityModel

---
+++ core/domain/src/main/java/com/example/domain/model/CharityModel.kt
@@ -0,0 +1,12 @@
+package com.example.domain.model
+
+data class CharityModel(
+    val id: Int,
+    val category: String,
+    val name: String,
+    val organization: String,
+    val goal: Int,
+    val current: Int,
+    val site: String?,
+    val phone: String?
+)
