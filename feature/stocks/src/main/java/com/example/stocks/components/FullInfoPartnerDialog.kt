2757aaa9dd469e2b88f0a2f01542dd4e96252514
qwertyAL <sergeyiksanov25@gmail.com>
2024-08-22 22:06:21 +0300

    update stocks ui

--- feature/stocks/src/main/java/com/example/stocks/components/FullInfoPartnerDialog.kt
+++ feature/stocks/src/main/java/com/example/stocks/components/FullInfoPartnerDialog.kt
@@ -1,5 +1,8 @@
 package com.example.stocks.components
 
+import android.annotation.SuppressLint
+import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -11,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -31,12 +35,14 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.window.Dialog
 import coil.compose.AsyncImage
 import com.example.domain.model.PartnersModel
 import com.example.stocks.R
 import com.example.ui.theme.mColors
 
+@SuppressLint("SetJavaScriptEnabled")
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun FullInfoPartnerDialog(
@@ -79,104 +85,121 @@ fun FullInfoPartnerDialog(
                     containerColor = mColors.surface
                 )
             ) {
-                var isLoading by remember {
-                    mutableStateOf(false)
-                }
-                var isError by remember {
-                    mutableStateOf(false)
-                }
+                LazyColumn {
+                    item {
+                        var isLoading by remember {
+                            mutableStateOf(false)
+                        }
+                        var isError by remember {
+                            mutableStateOf(false)
+                        }
 
-                Box(
-                    modifier = Modifier.fillMaxWidth(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    AsyncImage(
-                        model = partner.backgroundImage,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(140.dp)
-                            .background(Color(0xFFE3E3E3), RoundedCornerShape(25.dp))
-                            .clip(RoundedCornerShape(20.dp)),
-                        onLoading = {
-                            isLoading = true
-                        },
-                        onSuccess = {
-                            isLoading = false
-                        },
-                        onError = {
-                            isLoading = false
-                            isError = true
-                        },
-                        contentScale = ContentScale.Crop
-                    )
-                    if (isLoading) {
-                        CircularProgressIndicator(
-                            color = mColors.primary
-                        )
-                    }
-                    if (isError) {
-                        Icon(painter = painterResource(id = R.drawable.no_image_icon), contentDescription = null)
-                    }
-                }
-
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
-                ) {
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(
-                        text = partner.title,
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.Black
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    if (partner.dateEnd != null) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier
-                                .background(Color(0xFF00B545), RoundedCornerShape(8.dp))
-                                .padding(start = 4.dp, end = 4.dp, top = 2.dp, bottom = 2.dp)
+                        Box(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.Center
                         ) {
-                            Text(
-                                text = "Акция действует до ",
-                                color = Color.White,
-                                fontSize = 11.sp
+                            AsyncImage(
+                                model = partner.backgroundImage,
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(140.dp)
+                                    .background(Color(0xFFE3E3E3), RoundedCornerShape(25.dp))
+                                    .clip(RoundedCornerShape(20.dp)),
+                                onLoading = {
+                                    isLoading = true
+                                },
+                                onSuccess = {
+                                    isLoading = false
+                                },
+                                onError = {
+                                    isLoading = false
+                                    isError = true
+                                },
+                                contentScale = ContentScale.Crop
                             )
+                            if (isLoading) {
+                                CircularProgressIndicator(
+                                    color = mColors.primary
+                                )
+                            }
+                            if (isError) {
+                                Icon(painter = painterResource(id = R.drawable.no_image_icon), contentDescription = null)
+                            }
+                        }
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                        ) {
+                            Spacer(modifier = Modifier.height(8.dp))
                             Text(
-                                text = partner.dateEnd!!,
-                                color = Color.White,
-                                fontSize = 11.sp,
-                                fontWeight = FontWeight.Bold
+                                text = partner.title,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Black
                             )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            if (partner.dateEnd != null) {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier
+                                        .background(Color(0xFF00B545), RoundedCornerShape(8.dp))
+                                        .padding(
+                                            start = 4.dp,
+                                            end = 4.dp,
+                                            top = 2.dp,
+                                            bottom = 2.dp
+                                        )
+                                ) {
+                                    Text(
+                                        text = "Акция действует до ",
+                                        color = Color.White,
+                                        fontSize = 11.sp
+                                    )
+                                    Text(
+                                        text = partner.dateEnd!!,
+                                        color = Color.White,
+                                        fontSize = 11.sp,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                }
+                            }
+                            Spacer(modifier = Modifier.height(8.dp))
+
+                            AndroidView(
+                                factory = {
+                                    WebView(it).apply {
+                                        layoutParams = ViewGroup.LayoutParams(
+                                            ViewGroup.LayoutParams.MATCH_PARENT,
+                                            ViewGroup.LayoutParams.WRAP_CONTENT
+                                        )
+                                        settings.javaScriptEnabled = true
+                                        loadData(partner.description, "text/html", "UTF-8")
+                                    }
+                                },
+                                modifier = Modifier.fillMaxWidth()
+                            )
+
+                            if (partner.site != "" && partner.phone != "") {
+                                Spacer(modifier = Modifier.height(8.dp))
+                                Text(text = "Подробнее по ссылке", fontSize = 13.sp, color = Color.Black)
+                                Text(text = partner.site, color = mColors.primary, fontSize = 13.sp)
+                                Text(text = "или по телефону", fontSize = 13.sp, color = Color.Black)
+                                Text(text = partner.phone, color = mColors.primary, fontSize = 13.sp)
+                            } else if (partner.site != "") {
+                                Spacer(modifier = Modifier.height(8.dp))
+                                Text(text = "Подробнее по ссылке", fontSize = 13.sp, color = Color.Black)
+                                Text(text = partner.site, color = mColors.primary, fontSize = 13.sp)
+                            } else if (partner.phone != "") {
+                                Spacer(modifier = Modifier.height(8.dp))
+                                Text(text = "Подробнее по телефону", fontSize = 13.sp, color = Color.Black)
+                                Text(text = partner.phone, color = mColors.primary, fontSize = 13.sp)
+                            }
                         }
                     }
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(
-                        text = partner.description,
-                        fontSize = 13.sp,
-                        color = Color.Black
-                    )
-
-                    if (partner.site != "" && partner.phone != "") {
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(text = "Подробнее по ссылке", fontSize = 13.sp, color = Color.Black)
-                        Text(text = partner.site, color = mColors.primary, fontSize = 13.sp)
-                        Text(text = "или по телефону", fontSize = 13.sp, color = Color.Black)
-                        Text(text = partner.phone, color = mColors.primary, fontSize = 13.sp)
-                    } else if (partner.site != "") {
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(text = "Подробнее по ссылке", fontSize = 13.sp, color = Color.Black)
-                        Text(text = partner.site, color = mColors.primary, fontSize = 13.sp)
-                    } else if (partner.phone != "") {
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(text = "Подробнее по телефону", fontSize = 13.sp, color = Color.Black)
-                        Text(text = partner.phone, color = mColors.primary, fontSize = 13.sp)
-                    }
                 }
-
             }
         }
     }
