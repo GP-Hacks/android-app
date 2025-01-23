e693edc379e862088ff42ec52c7c0ced2189aac9
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-01-23 10:06:06 +0300

    add ksp plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     id("com.google.dagger.hilt.android") version "2.49" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13"
 }
